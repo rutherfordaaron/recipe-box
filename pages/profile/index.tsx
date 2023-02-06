15f4948be98fc77bd76e445f23391dd929ebddd1
Aaron Rutherford <rutherfordaaron1@gmail.com>
2023-02-05 16:57:05 -0800

    Loading State
    Created an initial loading state to use while getServerSideProps does its thing

--- pages/profile/index.tsx
+++ pages/profile/index.tsx
@@ -1,20 +1,37 @@
 import getUser from "../../util/getUser";
 import { GetServerSideProps, NextPage } from "next";
+import { useEffect, useState } from "react";
+import Loading from "../../components/loading";
+import { useRouter } from "next/router";
 
 type PageProps = {
   user: string,
 }
 
 const Profile: NextPage<PageProps> = (props) => {
+  const [loading, setLoading] = useState(true);
   const user = JSON.parse(props.user);
+  const router = useRouter();
 
-  return (
-    <>
-      <p>User: {user.username}</p>
-      <p>Email: {user.email}</p>
-      <p>Created: {user.created}</p>
-    </>
-  )
+  useEffect(() => {
+    if (router.isReady) {
+      setLoading(false);
+    }
+  }, [])
+
+  if (loading) {
+    return (
+      <Loading />
+    )
+  } else {
+    return (
+      <>
+        <p>User: {user.username}</p>
+        <p>Email: {user.email}</p>
+        <p>Created: {user.created}</p>
+      </>
+    )
+  }
 }
 
 export const getServerSideProps: GetServerSideProps<PageProps> = async (context) => {
