9720a5e7029aa8e9d3c704e76673147f0bdef115
Aaron Rutherford <rutherfordaaron1@gmail.com>
2023-02-21 19:05:22 -0800

    Removed unused code

--- pages/profile/my-recipe-box/new-recipe.tsx
+++ pages/profile/my-recipe-box/new-recipe.tsx
@@ -1,9 +1,7 @@
 import { useState } from "react";
-import Recipe, { RecipeType } from "../../../models/recipe";
 import { GetServerSideProps, NextPage } from "next";
 import getUser from "../../../util/getUser";
 import { GetUserProps } from "../../../util/getUser";
-import styles from "../../../public/styles/profile/new-recipe-form.module.scss";
 import PageOne from "../../../components/new-recipe-form/Page1";
 import PageTwo from "../../../components/new-recipe-form/Page2";
 import PageThree from "../../../components/new-recipe-form/Page3";
@@ -17,24 +15,6 @@ const NewRecipe: NextPage<GetUserProps> = (props) => {
   // State page variable to track which page of the form should render
   const [page, setPage] = useState(1);
   const [recipe, setRecipe] = useState({});
-
-  // State variables that will be used to create new recipe object
-  // ***** All of this will be moved into their own components *****
-
-  // // form page 2
-  // const [ingredients, setIngredients] = useState<undefined | string[][]>(undefined);
-
-  // // form page 3
-  // const [directions, setDirections] = useState<undefined | string[]>(undefined);
-
-  // // form page 4
-  // const [prepTime, setPrepTime] = useState<undefined | number>(undefined);
-  // const [cookTime, setCookTime] = useState<undefined | number>(undefined);
-  // const [servingsYield, setServingsYield] =
-  //   useState<[(number | undefined), (number | undefined)] | undefined>(undefined);
-
-  // // form page 5
-  // const [rating, setRating] = useState<undefined | number[]>(undefined);
 
   let owner = user.username;
 
