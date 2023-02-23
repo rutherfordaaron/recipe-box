4124b5fa6ee87ee93ff0f681192015e3390b57ac
Aaron Rutherford <rutherfordaaron1@gmail.com>
2023-02-23 13:54:51 -0800

    Restyles Input component with Tailwind
    Also changed the how it looks it general. Instead of a border all the way around, there is only a bottom border. It made it look much cleaner

--- components/input/input.tsx
+++ components/input/input.tsx
@@ -1,4 +1,3 @@
-import styles from "./input.module.scss";
 import { ChangeEventHandler } from "react";
 
 const Input = (props: {
@@ -12,10 +11,15 @@ const Input = (props: {
 }) => {
 
   return (
-    <div className="input-container">
+    <div className="relative my-4">
       <label
         htmlFor={props.id}
-        className={!props.valid && props.state ? styles.invalidLabel : props.state ? styles.active : ""}
+        className={`
+          absolute top-0 left-2 text-slate-200 transition-all
+          ${!props.valid && props.state ? "text-red-400 border-red-400 text-xs -top-[10px]"
+            : props.state ? "border-blue-300 text-blue-300 text-xs -top-[10px]"
+              : "bottom-0"}
+        `}
       >
         {props.label}
       </label>
@@ -25,7 +29,10 @@ const Input = (props: {
         id={props.id}
         name={props.id}
         onChange={props.onChange}
-        className={`${props.state ? styles.activeInput : ""} ${!props.valid ? styles.invalid : ""}`}
+        className={`
+          border-b focus:outline-none focus:border-blue-300 transition-all
+          ${!props.valid && props.state ? "border-red-400" : props.state ? "border-blue-400" : ""}
+        `}
         value={props.state}
         min={props.range ? props.range[0] : undefined}
         max={props.range ? props.range[1] : undefined}
