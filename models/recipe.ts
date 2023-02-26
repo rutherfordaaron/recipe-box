ac7d361b852090c9caaf62d7a31d58d7ac6e4f72
Aaron Rutherford <rutherfordaaron1@gmail.com>
2023-02-26 11:16:52 -0800

    Recipe is now a type instead of a class

--- models/recipe.ts
+++ models/recipe.ts
@@ -6,34 +6,17 @@ export enum RecipeType {
   Original = "ORIGINAL"
 }
 
-export default class Recipe {
-  constructor(
-    public name: string,
-    public description: string,
-    public owner: string,
-    public origin: string,
-    public recipeType: RecipeType,
-    public ingredients: string[][],
-    public directions: string[],
-    public prepTime: number | undefined,
-    public cookTime: number | undefined,
-    public servingsYield: number | undefined,
-    public rating: number[] | undefined,
-    public _id?: ObjectId
-  ) {
-    this.name = name;
-    this.description = description;
-    this.owner = owner;
-    this.origin = origin;
-    this.recipeType = recipeType;
-    this.ingredients = ingredients;
-    this.directions = directions;
-    this.prepTime = prepTime;
-    this.cookTime = cookTime;
-    this.servingsYield = servingsYield;
-    this.rating = rating;
-    if (_id) {
-      this._id = _id;
-    }
-  }
+export type Recipe = {
+  name: string,
+  description: string,
+  owner: string,
+  origin: string,
+  recipeType: RecipeType,
+  ingredients: string[][]
+  directions: string[],
+  prepTime?: number,
+  cookTime?: number,
+  servings?: number,
+  rating?: number[],
+  _id?: ObjectId
 }
