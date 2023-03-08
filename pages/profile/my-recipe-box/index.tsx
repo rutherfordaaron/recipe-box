8cffc0503202eb43b41558e47bd181d524a30cee
Aaron Rutherford <rutherfordaaron1@gmail.com>
2023-03-07 19:23:23 -0800

    Get user recipes array API endpoint
    Moved from recipes/get-user-recipes to /user-recipes. Also now included user validation to make sure you're authorized to get the recipe list from the database

--- pages/profile/my-recipe-box/index.tsx
+++ pages/profile/my-recipe-box/index.tsx
@@ -12,10 +12,11 @@ import RecipeCard from "../../../components/recipe-card/RecipeCard";
 const MyRecipeBox: NextPage<GetUserProps> = (props) => {
   const user = JSON.parse(props.user);
   const [loading, setLoading] = useState(true);
-  const [userRecipes, setUserRecipes] = useState<Record<string, any>[]>([])
+  const [userRecipes, setUserRecipes] = useState<Record<string, any>[] | string>([]);
+  const [error, setError] = useState(false);
 
   useEffect(() => {
-    fetch("/api/recipes/get-user-recipes", {
+    fetch("/api/user-recipes", {
       method: "GET",
       headers: {
         "user": user.username
@@ -23,42 +24,55 @@ const MyRecipeBox: NextPage<GetUserProps> = (props) => {
     })
       .then((response) => response.json())
       .then((data) => {
+        if (data.error) {
+          setError(true);
+        }
         setUserRecipes(data.data);
         setLoading(false);
+
       })
   }, [])
 
-  return (
-    <>
-      <h1>My Recipe Box</h1>
-      <div className="flex flex-col justify-center items-center gap-4">
-        {loading ? <Loading /> : userRecipes.map((el, i) => {
-          return (
-            <RecipeCard
-              key={`recipeCard${i}`}
-              id={el._id}
-              name={el.name}
-              description={el.description}
-              recipeType={el.recipeType}
-              origin={el.origin}
-              prepTime={el.prepTime}
-              cookTime={el.cookTime}
-              rating={el.rating}
-            />
-          )
-        })}
-      </div>
-      <Link
-        className="fixed bottom-5 right-5 text-5xl bg-sky-500 shadow-md w-20 h-20
-       flex justify-center items-center rounded-full 
-       hover:bg-sky-300 hover:shadow-none hover:bottom-4 hover:text-gray-800 
-       transition-all"
-        href="./my-recipe-box/new-recipe"
-      >
-        <FontAwesomeIcon icon={faPlus} />
-      </Link>
-    </>
-  )
+  if (!error) {
+    return (
+      <>
+        <h1>My Recipe Box</h1>
+        <div className="flex flex-col justify-center items-center gap-4">
+          {loading ? <Loading /> : typeof userRecipes !== "string" ? userRecipes.map((el, i) => {
+            return (
+              <RecipeCard
+                key={`recipeCard${i}`}
+                id={el._id}
+                name={el.name}
+                description={el.description}
+                recipeType={el.recipeType}
+                origin={el.origin}
+                prepTime={el.prepTime}
+                cookTime={el.cookTime}
+                rating={el.rating}
+              />
+            )
+          }) : <p>Something went wrong. Please try again</p>}
+        </div>
+        <Link
+          className="fixed bottom-5 right-5 text-5xl bg-sky-500 shadow-md w-20 h-20
+         flex justify-center items-center rounded-full 
+         hover:bg-sky-300 hover:shadow-none hover:bottom-4 hover:text-gray-800 
+         transition-all"
+          href="./my-recipe-box/new-recipe"
+        >
+          <FontAwesomeIcon icon={faPlus} />
+        </Link>
+      </>
+    )
+  } else {
+    return (
+      <>
+        <h1>Something went wrong!</h1>
+        <p>Error: {typeof userRecipes === "string" ? userRecipes : ""}</p>
+      </>
+    )
+  }
 }
 
 export const getServerSideProps: GetServerSideProps<GetUserProps> = async (context) => {
