319930c7794aeb01b4da8e98072e1979e5e125c7
Aaron Rutherford <rutherfordaaron1@gmail.com>
2023-03-27 13:55:36 -0700

    Updated to use util method getUser()

--- pages/profile/index.tsx
+++ pages/profile/index.tsx
@@ -1,41 +1,19 @@
 import getUser from "../../util/getUser";
-import { GetServerSideProps, NextPage } from "next";
-import { useEffect, useState } from "react";
 import Loading from "../../components/loading";
-import { useRouter } from "next/router";
 
-type PageProps = {
-  user: string,
-}
+const Profile = () => {
+  let { userData, userError, userIsLoading } = getUser();
 
-const Profile: NextPage<PageProps> = (props) => {
-  const [loading, setLoading] = useState(true);
-  const user = JSON.parse(props.user);
-  const router = useRouter();
+  if (userIsLoading) return <Loading />
+  if (userError) return <p>Something went wrong! {userError.message}</p>
 
-  useEffect(() => {
-    if (router.isReady) {
-      setLoading(false);
-    }
-  }, [])
-
-  if (loading) {
-    return (
-      <Loading />
-    )
-  } else {
-    return (
-      <>
-        <p>User: {user.username}</p>
-        <p>Email: {user.email}</p>
-        <p>Created: {user.created}</p>
-      </>
-    )
-  }
-}
-
-export const getServerSideProps: GetServerSideProps<PageProps> = async (context) => {
-  return await getUser(context);
+  if (userData && userData.user) return (
+    <>
+      <p>User: {userData.user.username}</p>
+      <p>Email: {userData.user.email}</p>
+      <p>Created: {userData.user.created.toString()}</p>
+    </>
+  )
 }
 
 export default Profile;
