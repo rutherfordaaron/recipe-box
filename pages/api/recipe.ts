0bb3ebb39ff8a0d752d16c5714eae7cae3cc8f65
Aaron Rutherford <rutherfordaaron1@gmail.com>
2023-03-27 16:57:56 -0700

    Updated to match standard API return data structure
    Only updates the GET method. Still need to fix up the POST method

--- pages/api/recipe.ts
+++ pages/api/recipe.ts
@@ -2,74 +2,67 @@ import type { NextApiRequest, NextApiResponse } from "next";
 import clientPromise from "../../util/db";
 import getUser from "../../util/getUser";
 import { ObjectId } from "mongodb";
+import getToken from "../../util/getToken";
 
 const handler = async (req: NextApiRequest, res: NextApiResponse) => {
   // Connect to the "recipes" collection on the database
   const client = await clientPromise;
   const db = client.db(process.env.DB);
   const recipes = db.collection("recipes");
+  const token = getToken(req);
 
   switch (req.method) {
     /* -------------------- GET -------------------- */
     // Get a recipe from the database
     case "GET":
-      // if id header isn't foud, 400: Bad Request
-      const id = req.headers["id"];
+      // if id property isn't foud, 400: Bad Request
+      const id = req.query.id;
       if (!id) {
-        res.status(400).json({ error: true, data: "missing data to process request" })
+        res.status(400).json({ recipe: null, message: "missing data to process request" })
         break;
       }
 
-      // Get recipe from given id parameter from request headers
+      // Get recipe from given id parameter from request body
       const recipe = await recipes.findOne({ _id: new ObjectId(String(id)) });
 
       // if recipe is not found in database, 404: Not Found
       if (!recipe) {
-        res.status(404).json({ error: true, data: "item not found" });
+        res.status(404).json({ recipe: null, message: "recipe not found" });
         break;
       }
 
       // If recipe is not marked as public, verify user before sending back the recipe
-      // Will reqire a username and token be provided
       if (!recipe.public) {
-        const user = req.headers["user"];
-        const token = req.headers["token"];
 
-        // If either user or token header is not provided, 400: Bad Request
-        if (!user || !token) {
-          res.status(400).json({ error: true, data: "missing data to process request" });
+        // If token is not provided, 400: Bad Request
+        if (!token) {
+          res.status(400).json({ reicpe: null, message: "missing authentication token" });
           break;
         }
 
         // VERIFY THE USER
         const users = db.collection("users");
-        const userToValidate = await users.findOne({ username: user });
-        // If no use can be found from provided username, 400: Bad Request
+        const userToValidate = await users.findOne({ token: token });
+        // If no user can be found from provided username, 400: Bad Request
         if (!userToValidate) {
-          res.status(400).json({ error: true, data: "no user found" });
-          break;
-        }
-
-        // If user database token and provided token don't match, 401: Unauthorized
-        if (userToValidate.token !== token) {
-          res.status(401).json({ error: true, data: "unauthorized" });
+          res.status(400).json({ recipe: null, message: "no user found from provided authentication token" });
           break;
         }
 
         // If recipe owner and user don't match up, 401: unauthorized
         if (recipe.owner !== userToValidate.username) {
-          res.status(401).json({ error: true, data: "unauthorized" });
+          res.status(401).json({ recipe: null, message: "unauthorized" });
           break;
         }
 
         // If user exists, and the correct token is given, send the recipe
         console.log(recipe);
-        res.status(200).json({ error: false, data: recipe });
+        res.status(200).json({ message: "success", recipe: recipe });
         break;
       }
 
       // If recipe is marked as public, send it
-      res.status(200).json({ error: false, data: recipe });
+      res.status(200).json({ message: "success", recipe: recipe });
       break;
     /* -------------------- POST -------------------- */
     // Insert a new recipe into the database
@@ -78,12 +71,6 @@ const handler = async (req: NextApiRequest, res: NextApiResponse) => {
       const recipeData = req.headers["recipe"];
       if (typeof recipeData === "string") {
         const newRecipe = JSON.parse(recipeData);
-
-        const userData = await getUser({ req: req });
-        if (userData.hasOwnProperty("redirect")) {
-          res.status(401).json({ error: true, data: "unauthorized" });
-          break;
-        }
 
         // Insert new recipe
         const recipes = db.collection("recipes");
