c4fd27e3e214478e18a3bbb0f494930eb62c3f43
Aaron Rutherford <rutherfordaaron1@gmail.com>
2023-03-27 17:02:29 -0700

    Removed breaking code
    Accidently left over form an experiment

--- util/getRecipe.ts
+++ util/getRecipe.ts
@@ -2,12 +2,12 @@ import useSWR from "swr";
 import { GetRecipeAPIData } from "./types";
 
 const fetcher = async (route: string, id: string): Promise<GetRecipeAPIData> => {
-  const data = await fetch(route, { method: "GET", body: "" }).then(res => res.json());
+  const data = await fetch(route, { method: "GET" }).then(res => res.json());
   return data;
 }
 
 const getRecipe = (id: string) => {
-  let { data, error, isLoading } = useSWR<GetRecipeAPIData, Error>([`/api/recipe?id=${id}`, id], fetcher);
+  let { data, error, isLoading } = useSWR<GetRecipeAPIData, Error>(`/api/recipe?id=${id}`, fetcher);
 
   return {
     recipeData: data,
