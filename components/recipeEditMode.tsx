5c78c99ab5c8548bfc0476c2f36cbe4ce0729ac6
Aaron Rutherford <rutherfordaaron1@gmail.com>
2023-03-31 12:33:07 -0700

    Cancel and save
    Cancel to go back to recipe without saving changes. Save button redirects back to the my-recipe-box page

--- components/recipeEditMode.tsx
+++ components/recipeEditMode.tsx
@@ -1,10 +1,22 @@
 import { Recipe } from "../util/types";
+import { useRouter } from "next/router";
 
 const RecipeEditMode = (props: { recipe: Recipe, setEditMode: Function }) => {
+  const router = useRouter();
+
+  const updateRecipe = () => {
+    router.push({ pathname: "/profile/my-recipe-box", query: { message: "Recipe updated", good: true } }, "/profile/my-recipe-box");
+  }
+
+  const cancelEdit = () => {
+    props.setEditMode(false);
+  }
+
   return (
     <>
       <h1>Editing recipe {props.recipe._id?.toString()}</h1>
-      <button type="button" onClick={e => props.setEditMode(false)}>Save</button>
+      <button type="button" onClick={updateRecipe}>Save</button>
+      <button type="button" onClick={cancelEdit}>Cancel</button>
     </>
   )
 }
