dddb4cf7e960072e4ee24b4646d901be6aca74e6
Aaron Rutherford <rutherfordaaron1@gmail.com>
2023-04-01 10:09:26 -0700

    Spinner animation
    Screen sized and makes the rest of the site uninteractable with a loading overlay

--- components/loading.tsx
+++ components/loading.tsx
@@ -1,6 +1,11 @@
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faSpinner } from "@fortawesome/free-solid-svg-icons";
+
 const Loading = () => {
   return (
-    <h1>Loading...</h1>
+    <div className="fixed inset-0 bg-white opacity-80 text-3xl z-[5] flex justify-center items-center">
+      <FontAwesomeIcon icon={faSpinner} className="animate-spin" />
+    </div>
   )
 }
 
