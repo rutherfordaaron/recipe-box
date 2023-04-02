01a2adbe7f1b3bcdd85c1b37d00dd4fe1582acca
Aaron Rutherford <rutherfordaaron1@gmail.com>
2023-04-01 19:39:00 -0700

    Responsive for larger screens

--- pages/profile/my-recipe-box/index.tsx
+++ pages/profile/my-recipe-box/index.tsx
@@ -13,26 +13,28 @@ const MyRecipeBox = () => {
 
   return (
     <>
-      <h1>My Recipe Box</h1>
-      <div className="flex flex-col justify-center items-center gap-4">
-        {isLoading ? <Loading /> : error ? <h1>Error</h1> : data && data.recipes ? data.recipes.map((el, i) => {
-          return (
-            <RecipeCard
-              recipe={el}
-              key={el._id?.toString()}
-            />
-          )
-        }) : <></>}
-      </div>
-      <Link
-        className="fixed bottom-5 right-5 text-5xl bg-sky-500 shadow-md w-20 h-20
+      <div className="max-w-[1000px] mx-auto">
+        <h1 className="text-center">My Recipe Box</h1>
+        <div className="flex flex-col md:grid lg:grid-cols-2 justify-center items-center gap-4">
+          {isLoading ? <Loading /> : error ? <h1>Error</h1> : data && data.recipes ? data.recipes.map((el, i) => {
+            return (
+              <RecipeCard
+                recipe={el}
+                key={el._id?.toString()}
+              />
+            )
+          }) : <></>}
+        </div>
+        <Link
+          className="fixed bottom-5 right-5 text-5xl bg-sky-200 shadow-md w-20 h-20
        flex justify-center items-center rounded-full 
        hover:bg-sky-300 hover:shadow-none hover:bottom-4 hover:text-gray-800 
        transition-all"
-        href="./my-recipe-box/new-recipe"
-      >
-        <FontAwesomeIcon icon={faPlus} />
-      </Link>
+          href="./my-recipe-box/new-recipe"
+        >
+          <FontAwesomeIcon icon={faPlus} />
+        </Link>
+      </div>
     </>
   )
 
