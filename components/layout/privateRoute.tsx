32aa3bc3d74b082f19362011a76dd304acfc8619
Aaron Rutherford <rutherfordaaron1@gmail.com>
2023-04-01 20:09:42 -0700

    User limbo fix
    User would be in limbo if logged in on one device then logged in on another device. The first device would act as if logged in and not redirect on protected routes. This should fix that.

--- components/layout/privateRoute.tsx
+++ components/layout/privateRoute.tsx
@@ -2,16 +2,24 @@ import { useRouter } from "next/router";
 import { useEffect, useState } from "react";
 import { useCookies } from "react-cookie";
 import Loading from "../loading";
+import getUser from "../../util/getUser";
 
 export const PrivateRoute = (props: { children: JSX.Element }) => {
   const router = useRouter();
-  const [path, setPath] = useState(router.pathname);
+  const { userData, userError, userIsLoading } = getUser();
   const [loading, setLoading] = useState(true);
-  const [children, setChildren] = useState(props.children)
   const [cookies] = useCookies(["token"]);
-  const token = cookies.token;
 
-
+  const redirect = () => {
+    router.push({
+      pathname: "/login",
+      query: {
+        message: "Please login before viewing this page",
+        good: false,
+        returnTo: router.pathname
+      }
+    }, "/login");
+  }
 
   useEffect(() => {
     const unprotectedRoutes = [
@@ -21,21 +29,13 @@ export const PrivateRoute = (props: { children: JSX.Element }) => {
     ]
 
     setLoading(false);
-    setPath(router.pathname);
-    if (!token && !unprotectedRoutes.find(e => e == router.pathname)) {
+    if (((userData && !userData.user) || !cookies.token) && !unprotectedRoutes.find(e => e == router.pathname)) {
+      redirect();
       setLoading(true);
-      router.push({
-        pathname: "/login",
-        query: {
-          message: "Please login before viewing this page",
-          good: false,
-          returnTo: router.pathname
-        }
-      }, "/login");
-
     }
-  }, [router, token])
+  }, [router.pathname, cookies.token, userData, userIsLoading])
 
+  if (userIsLoading) return <Loading />
   return (
     <>
       {loading ? <Loading /> : props.children}
