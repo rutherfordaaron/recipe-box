16debfb0788a061f0e4be34ef658cbaa7a3e4fa0
Aaron Rutherford <rutherfordaaron1@gmail.com>
2023-04-03 19:11:40 -0700

    Minor styling changes

--- components/filterMenu.tsx
+++ components/filterMenu.tsx
@@ -19,12 +19,13 @@ export const FilterMenu = (props: { showFilterMenu: boolean, setShowFilterMenu: 
   }
 
   return (
-    <div className={`fixed top-0 bottom-0 right-0 bg-sky-50 z-50 flex flex-col gap-4 w-[55%] max-w-[500px] ${showFilterMenu ? "" : "hidden"}`}>
+    <div className={`fixed top-0 bottom-0 right-0 bg-sky-50 z-50 flex flex-col gap-4 w-3/4 md:w-1/2 max-w-[500px] px-4 pt-16 ${showFilterMenu ? "" : "hidden"}`}>
       <button onClick={e => setShowFilterMenu(false)} type="button" className="shadow-none absolute top-5 right-5 ">
         <FontAwesomeIcon icon={faX} />
       </button>
+      <p>Filter by tag:</p>
       {loading ? <Spinner /> : error ? <p>Error: {error.message}</p> : data && data.user ?
-        <div className="grid grid-cols-2 gap-2 w-4/6 mx-auto mt-16">
+        <div className="grid grid-cols-2 gap-2 w-full mx-auto">
           {data.user.tags ? data.user.tags.map((el, i) => {
             // Active Tag Filters
             if (tagFilter.find(item => item === el)) return (
