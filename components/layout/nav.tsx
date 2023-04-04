0df3039b566aa77429848aaa6f1bc559b5c8a700
Aaron Rutherford <rutherfordaaron1@gmail.com>
2023-04-03 20:24:47 -0700

    Update nav.tsx

--- components/layout/nav.tsx
+++ components/layout/nav.tsx
@@ -1,12 +1,14 @@
 import { motion } from "framer-motion"
+import { useRouter } from "next/router"
 
 type Props = {
   children: JSX.Element[]
 }
 
 const Nav = ({ children }: Props) => {
+  const router = useRouter();
   return (
-    <motion.nav className="hidden absolute top-0 right-0 flex-col bg-sky-300 p-4 pt-10 w-52 shadow-xl" id="nav">
+    <motion.nav key={router.pathname} className="hidden absolute top-0 right-0 flex-col bg-sky-300 p-4 pt-10 w-52 shadow-xl" id="nav">
       {children}
     </motion.nav>
   )
