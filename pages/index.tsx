408d316ec6d79fccc072b15fe50dd8f4561e4b03
Aaron Rutherford <rutherfordaaron1@gmail.com>
2023-04-05 05:51:35 -0700

    Login and signup buttons

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,7 +4,10 @@ export default function Home() {
   return (
     <>
       <h1 className="text-3xl font-bold">Welcome to Recipe Box!</h1>
-
+      <div className="flex gap-4">
+        <Link href="/login" className="bg-sky-300 shadow-lg rounded py-2 px-4 hover:bg-sky-200 transition-all">Login</Link>
+        <Link href="/sign-up" className="bg-sky-300 shadow-lg rounded py-2 px-4 hover:bg-sky-200 transition-all">Signup</Link>
+      </div>
     </>
   )
 }
