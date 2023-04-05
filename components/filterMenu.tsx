14a98c0e53319ca8bbbc377603e461f959bdb229
Aaron Rutherford <rutherfordaaron1@gmail.com>
2023-04-05 05:50:19 -0700

    Animates in

--- components/filterMenu.tsx
+++ components/filterMenu.tsx
@@ -2,6 +2,7 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import getUser from "../util/getUser"
 import { Spinner } from "./spinner";
 import { faX } from "@fortawesome/free-solid-svg-icons";
+import { AnimatePresence, motion } from "framer-motion";
 
 export const FilterMenu = (props: { showFilterMenu: boolean, setShowFilterMenu: Function, tagFilter: string[], setTagFilter: Function }) => {
   const { showFilterMenu, setShowFilterMenu, tagFilter, setTagFilter } = props;
@@ -18,38 +19,53 @@ export const FilterMenu = (props: { showFilterMenu: boolean, setShowFilterMenu: 
     setTagFilter(activeTags.filter(item => item !== value));
   }
 
+  const resetFilters = () => {
+    setTagFilter([]);
+  }
+
   return (
-    <div className={`fixed top-0 bottom-0 right-0 bg-sky-50 z-50 flex flex-col gap-4 w-3/4 md:w-1/2 max-w-[500px] px-4 pt-16 ${showFilterMenu ? "" : "hidden"}`}>
-      <button onClick={e => setShowFilterMenu(false)} type="button" className="shadow-none absolute top-5 right-5 ">
-        <FontAwesomeIcon icon={faX} />
-      </button>
-      <p>Filter by tag:</p>
-      {loading ? <Spinner /> : error ? <p>Error: {error.message}</p> : data && data.user ?
-        <div className="grid grid-cols-2 gap-2 w-full mx-auto">
-          {data.user.tags ? data.user.tags.map((el, i) => {
-            // Active Tag Filters
-            if (tagFilter.find(item => item === el)) return (
-              <button
-                key={`tagFilter${i}`}
-                className={`bg-sky-300 shadow`}
-                onClick={e => removeFilterTag(el)}
-              >
-                {el}
-              </button>
-            )
-            // Inactive Tag Filters
-            return (
-              <button
-                key={`tagFilter${i}`}
-                className={`bg-sky-100 shadow`}
-                onClick={e => addFilterTag(el)}
-              >
-                {el}
-              </button>
-            )
-          }) : <></>}
-        </div>
-        : <></>}
-    </div>
+    <AnimatePresence>
+      {showFilterMenu ?
+        <motion.div
+          key="filterMenu"
+          className={`fixed top-5 bottom-5 rounded-l right-0 bg-sky-50 z-50 flex flex-col gap-4 w-3/4 md:w-1/2 max-w-[500px] px-4 pt-16 shadow-2xl`}
+          initial={{ opacity: 0, x: 500 }}
+          animate={{ x: 0, opacity: 1 }}
+          exit={{ x: 500, opacity: 0 }}
+          transition={{ spring: 0 }}
+        >
+          <button onClick={e => setShowFilterMenu(false)} type="button" className="shadow-none absolute top-5 right-5 ">
+            <FontAwesomeIcon icon={faX} />
+          </button>
+          <p>Filter by tag:</p>
+          {loading ? <Spinner /> : error ? <p>Error: {error.message}</p> : data && data.user ?
+            <div className="grid grid-cols-2 gap-2 w-full mx-auto">
+              {data.user.tags ? data.user.tags.map((el, i) => {
+                // Active Tag Filters
+                if (tagFilter.find(item => item === el)) return (
+                  <button
+                    key={`tagFilter${i}`}
+                    className={`bg-sky-300 shadow`}
+                    onClick={e => removeFilterTag(el)}
+                  >
+                    {el}
+                  </button>
+                )
+                // Inactive Tag Filters
+                return (
+                  <button
+                    key={`tagFilter${i}`}
+                    className={`bg-sky-100 shadow`}
+                    onClick={e => addFilterTag(el)}
+                  >
+                    {el}
+                  </button>
+                )
+              }) : <></>}
+            </div>
+            : <></>}
+          <button type="button" className="mx-auto" onClick={resetFilters}>Reset filters</button>
+        </motion.div> : <></>}
+    </AnimatePresence>
   )
 }
--- components/sortMenu.tsx
+++ components/sortMenu.tsx
@@ -1,20 +1,31 @@
+import { AnimatePresence, motion } from "framer-motion";
 import { SortParameter } from "../util/types";
 
 export const SortMenu = (props: { setSort: Function, activeSortParameter: SortParameter, visible: boolean, setVisible: Function }) => {
   const { setSort, activeSortParameter, visible, setVisible } = props;
   const sortArr = [SortParameter.Ascending, SortParameter.Descending, SortParameter.RecentlyCreated, SortParameter.RecentlyUpdated, SortParameter.OldestCreated, SortParameter.OldestUpdated]
   return (
-    <div className={`absolute top-14 -left-3 bg-sky-50 ${visible ? "" : "hidden"} shadow-lg rounded overflow-hidden z-40 flex flex-col`}>
-      {sortArr.map((el, i) => {
-        return (
-          <button key={`sortParameter${i}`} onClick={e => {
-            setSort(el);
-            setVisible(false);
-          }} className={`${el == activeSortParameter ? "bg-sky-300" : ""} rounded-none border-b-2 shadow-none border-sky-100`}>
-            {el}
-          </button>
-        )
-      })}
-    </div>
+    <AnimatePresence>
+      {visible ?
+        <motion.div
+          key="sortMenu"
+          className={`absolute top-14 -left-3 bg-sky-50 shadow-lg rounded overflow-hidden z-40 flex flex-col`}
+          initial={{ opacity: 0, x: -100 }}
+          animate={{ opacity: 1, x: 0 }}
+          exit={{ opacity: 0, x: -100 }}
+          transition={{ spring: 0 }}
+        >
+          {sortArr.map((el, i) => {
+            return (
+              <button key={`sortParameter${i}`} onClick={e => {
+                setSort(el);
+                setVisible(false);
+              }} className={`${el == activeSortParameter ? "bg-sky-300" : ""} rounded-none border-b-2 shadow-none border-sky-100`}>
+                {el}
+              </button>
+            )
+          })}
+        </motion.div> : <></>}
+    </AnimatePresence>
   )
 }
