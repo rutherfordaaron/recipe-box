f5a2e10e19645f9b81cf1b5216dd8721183e2b5f
Aaron Rutherford <rutherfordaaron1@gmail.com>
2023-04-06 08:07:19 -0700

    Only show login button if not logged in

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,19 +1,22 @@
 import Link from "next/link";
 import getPublicRecipes from "../util/getPublicRecieps";
 import { Spinner } from "../components/spinner";
-import { Recipe } from "../util/types";
 import RecipeCard from "../components/RecipeCard";
+import getUser from "../util/getUser";
 
 export default function Home() {
   const { publicRecipesData: data, publicRecipesError: error, publicRecipesIsLoading: loading } = getPublicRecipes();
 
+  const { userData } = getUser();
+
   return (
     <>
       <h1 className="text-3xl font-bold">Welcome to Recipe Box!</h1>
-      <div className="flex gap-4">
-        <Link href="/login" className="bg-sky-300 shadow-lg rounded py-2 px-4 hover:bg-sky-200 transition-all">Login</Link>
-        <Link href="/sign-up" className="bg-sky-300 shadow-lg rounded py-2 px-4 hover:bg-sky-200 transition-all">Signup</Link>
-      </div>
+      {!(userData && userData.user) ?
+        <div className="flex gap-4">
+          <Link href="/login" className="bg-sky-300 shadow-lg rounded py-2 px-4 hover:bg-sky-200 transition-all">Login</Link>
+          <Link href="/sign-up" className="bg-sky-300 shadow-lg rounded py-2 px-4 hover:bg-sky-200 transition-all">Signup</Link>
+        </div> : <></>}
       <div className="mt-8">
         {loading ? <Spinner /> : error ? <p>Error: {error.message}</p> : data && data.recipes ?
           <div className="grid grid-cols-1 gap-4">
