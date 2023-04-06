64fdb023c164970597433be63de23646d7d8d7a3
Aaron Rutherford <rutherfordaaron1@gmail.com>
2023-04-06 11:26:42 -0700

    Escaped apostrophe

--- pages/index.tsx
+++ pages/index.tsx
@@ -14,7 +14,7 @@ export default function Home() {
       <div className="flex justify-center my-4">
         <Link href="/sign-up" className="bg-sky-300 shadow-lg rounded py-2 px-4 hover:bg-sky-200 transition-all">Get Started</Link>
       </div>
-      <p className="text-center">Want to see what it's all about?</p>
+      <p className="text-center">Want to see what it&apos;s all about?</p>
       <div className="flex justify-center my-4">
         <Link href="/public-recipes" className="bg-sky-300 shadow-lg rounded py-2 px-4 hover:bg-sky-200 transition-all">View Public Recipes</Link>
       </div>
