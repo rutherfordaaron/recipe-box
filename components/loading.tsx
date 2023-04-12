00018e7315fc1420561e183828f8f27ccc6dd6c4
Aaron Rutherford <rutherfordaaron1@gmail.com>
2023-04-12 06:21:56 -0700

    Prevent loading component to take up entire screen

--- components/loading.tsx
+++ components/loading.tsx
@@ -4,7 +4,9 @@ import { faSpinner } from "@fortawesome/free-solid-svg-icons";
 const Loading = () => {
   return (
     <div className="fixed inset-0 bg-white opacity-80 text-3xl z-[5] flex justify-center items-center">
-      <FontAwesomeIcon icon={faSpinner} className="animate-spin" />
+      <div className="max-w-[40px] mx-auto">
+        <FontAwesomeIcon icon={faSpinner} className="animate-spin" />
+      </div>
     </div>
   )
 }
