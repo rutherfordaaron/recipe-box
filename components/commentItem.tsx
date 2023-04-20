6b54dbdbc36702e11c4f5ba7c63d85610c6ebcbb
Aaron Rutherford <rutherfordaaron1@gmail.com>
2023-04-19 17:25:20 -0700

    Reply button now shows input
    Front end for commenting on comments

--- components/commentItem.tsx
+++ components/commentItem.tsx
@@ -3,10 +3,14 @@ import { useState } from "react";
 import CommentPreview from "./commentPreview";
 import { motion } from "framer-motion";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faReply } from "@fortawesome/free-solid-svg-icons";
+import { faMailReply, faReply } from "@fortawesome/free-solid-svg-icons";
+import Input from "./input";
+import { faPaperPlane } from "@fortawesome/free-regular-svg-icons";
 
 const CommentItem = (props: { el: Comment, depth: number }) => {
   const [showComments, setShowComments] = useState(false);
+  const [showReplyInput, setShowReplyInput] = useState(false);
+  const [reply, setReply] = useState("");
   const { el } = props;
 
   const buttonStyle = "transition-all shadow-none hover:bg-transparent hover:text-sky-600"
@@ -21,12 +25,32 @@ const CommentItem = (props: { el: Comment, depth: number }) => {
       <p className="text-sky-300 text-sm">{el.user}</p>
       <p className="pl-6 my-2 text-sm text-gray-600">{el.body}</p>
       <div className="flex gap-2 items-center text-sky-300">
-        <button className={`${buttonStyle} text-xs`}><FontAwesomeIcon icon={faReply} /></button>
+        <button onClick={e => setShowReplyInput(!showReplyInput)} className={`${buttonStyle} text-xs`}>
+          <FontAwesomeIcon icon={faReply} />
+        </button>
 
         <button onClick={e => setShowComments(!showComments)} className={buttonStyle}>
           <CommentPreview count={el.comments ? el.comments.length : 0} />
         </button>
       </div>
+
+      {showReplyInput ?
+        <div className="flex gap-2 items-center pl-4">
+          <div>
+            <Input
+              id={`comment${el._id}`}
+              type="textarea"
+              label="Your comment"
+              state={reply}
+              onChange={e => setReply(e.target.value)}
+              valid={!!reply}
+            />
+          </div>
+          <button className="shadow-none text-sky-400 hover:bg-transparent hover:text-sky-600">
+            <FontAwesomeIcon icon={faPaperPlane} />
+          </button>
+        </div> : <></>}
+
       {!showComments || !el.comments ? <></> : el.comments.map((el, i) => {
         return (
           <CommentItem key={`${props.depth + 1}comment${i}`} depth={props.depth + 1} el={el} />
