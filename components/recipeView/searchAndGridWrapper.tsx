43be7c75433c0077fa4ef26be88c60e63303f6c3
Aaron Rutherford <rutherfordaaron1@gmail.com>
2023-04-27 14:29:11 -0700

    Infinite Scroll
    Pagination happens on scroll with a spinner display to show if more content can come.

--- components/recipeView/searchAndGridWrapper.tsx
+++ components/recipeView/searchAndGridWrapper.tsx
@@ -1,9 +1,9 @@
 import { PropsWithChildren } from "react"
 import Search from "./search"
-import { Spinner } from "../spinner"
-import { useState, useEffect } from "react"
+import { useState, useEffect, useRef } from "react"
 import { Filter, emptyFilter } from "../../util/types"
 import RecipeGridPage from "./recipeGridPage"
+import { Spinner } from "../spinner"
 
 const SearchAndGridWrapper = (props: PropsWithChildren) => {
   const [filter, setFilter] = useState<Filter>(emptyFilter)
@@ -12,10 +12,12 @@ const SearchAndGridWrapper = (props: PropsWithChildren) => {
   const [page, setPage] = useState<number>(0);
   const [recipes, setRecipes] = useState<JSX.Element[]>([])
 
+  const gridInnerRef = useRef();
+
   useEffect(() => {
     let recipePageArr = [];
     for (let i = 0; i <= page; i++) {
-      recipePageArr.push(<RecipeGridPage index={i} size={4} filter={filter} setEndOfData={setEndOfData} />)
+      recipePageArr.push(<RecipeGridPage index={i} size={20} filter={filter} setEndOfData={setEndOfData} />)
     }
     setRecipes(recipePageArr);
   }, [page])
@@ -23,9 +25,16 @@ const SearchAndGridWrapper = (props: PropsWithChildren) => {
   useEffect(() => {
     setPage(0);
     let recipePageArr = [];
-    recipePageArr.push(<RecipeGridPage index={0} size={4} filter={filter} setEndOfData={setEndOfData} />)
+    recipePageArr.push(<RecipeGridPage index={0} size={20} filter={filter} setEndOfData={setEndOfData} />)
     setRecipes(recipePageArr);
   }, [newFilter])
+
+  const onScroll = () => {
+    if (gridInnerRef.current) {
+      const { scrollTop, scrollHeight, clientHeight } = gridInnerRef.current;
+      if (scrollTop + clientHeight >= scrollHeight && !endOfData) setPage(page + 1)
+    }
+  }
 
 
   return (
@@ -33,12 +42,14 @@ const SearchAndGridWrapper = (props: PropsWithChildren) => {
       <Search setNewFilter={setNewFilter} filter={filter} setFilter={setFilter} />
       <div
         className="flex flex-col md:grid lg:grid-cols-2 items-center gap-4 max-h-[73vh] md:max-h-[75vh] py-4 pr-2 overflow-y-scroll"
+        onScroll={onScroll}
+        //@ts-ignore
+        ref={gridInnerRef}
       >
         {recipes}
-        {/* <div className="col-span-2 py-8">
-        {endOfData ? <></> : <Spinner />}
-      </div> */}
-        <button className={`col-span-2 block w-[200px] mx-auto ${endOfData ? "hidden" : ""}`} onClick={e => setPage(page + 1)}>Show More</button>
+        <div className="col-span-2 pt-4 h-[50px] overflow-hidden">
+          {endOfData ? <></> : <Spinner />}
+        </div>
       </div>
     </div>
   )
